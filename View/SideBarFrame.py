2b008c068ebfb878af58a5eda7a73ff6fc5bc794
Trevor Bender <tbender@ptd.net>
2023-03-17 11:48:19 -0400

    Added logging messages to GUI

--- View/SideBarFrame.py
+++ View/SideBarFrame.py
@@ -5,15 +5,18 @@ Description : Contains the visuals for the side bar in the GUI
 import customtkinter as ctk
 from PIL import Image
 
+from fileLogging import logger
+
 class SideBarFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master, width=350, corner_radius=0)
+        logger().info(f"Creating side bar frame")
         self.grid(row=0, column=0, rowspan=4, sticky='nsew')
 
         self.grid_rowconfigure(5, weight=2)
-
+        logger().info(f"Adding button icons")
         # Home Button
-        self.homeButton = ctk.CTkButton(self, image=ctk.CTkImage(light_image=Image.open("./icons/icons8-home-24-black.png"), dark_image=Image.open("./icons/icons8-home-24-white.png"), size=(30, 30)), fg_color=("gray86", "gray17"), text="", width=75)
+        self.homeButton = ctk.CTkButton(self, image=ctk.CTkImage(light_image=Image.open("./icons/icons8-home-24-black.png"), dark_image=Image.open("./icons/icons8-home-24-white.png"), size=(30, 30)), fg_color=("gray86", "gray17"), text="", width=75, hover_color="#F5D547")
         self.homeButton.grid(row=0, column=0, padx=10, pady=(10, 10))
         # Search Button
         self.searchButton = ctk.CTkButton(self, image=ctk.CTkImage(light_image=Image.open("./icons/icons8-search-24-black.png"), dark_image=Image.open("./icons/icons8-search-24.png"), size=(30, 30)), fg_color=("gray86", "gray17"), text="", width=75)
@@ -30,3 +33,9 @@ class SideBarFrame(ctk.CTkFrame):
         # Add New Vehicle Button
         self.addNewButton = ctk.CTkButton(self, image=ctk.CTkImage(light_image=Image.open("./icons/icons8-add-new-26-black.png"), dark_image=Image.open("./icons/icons8-add-new-26.png"), size=(30, 30)), fg_color=("gray86", "gray17"), text="", width=75)
         self.addNewButton.grid(row=6, column=0, padx=10, pady=20)
+
+        # Test logo icon
+        # self.autoIcon = ctk.CTkButton(self, image=ctk.CTkImage(dark_image=Image.open("./icons/Method Draw Image(4).png"), size=(30, 30)), fg_color=("gray86", "gray17"), text="", width=75)
+        # self.autoIcon.grid(row=7, column=0, padx=10, pady=20)
+
+        logger().info(f"Successfully created side bar frame")
--- View/View.py
+++ View/View.py
@@ -1,32 +1,52 @@
 """
 Filename    : View.py
-Description :
+Description : Main container file for the GUI view
 """
 # Local imports
 from fileLogging import logger
+# View imports
 import View.SideBarFrame as SideBarFrame
 
 import customtkinter as ctk
 class View(ctk.CTk):
 
     def __init__(self, controller):
+        logger().info(f"Starting GUI")
         super().__init__()
         self.controller = controller
         self.title('Autodex')
         ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('green')
 
+        logger().info(f"Appearance: {ctk.get_appearance_mode()}")
+        # Creates a 4x4 grid layout
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2,3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         # Left side bar frame
         self.sideBar = SideBarFrame.SideBarFrame(self)
+        # Holds the toggle switch value for light/dark mode
+        self.appearance = ctk.StringVar(value=ctk.get_appearance_mode())
+        # Light/Dark mode toggle switch
+        self.appearanceSwitch = ctk.CTkSwitch(self, text="Appearance", variable=self.appearance, onvalue="Dark", offvalue="Light", command=self.toggleAppearance)
+        self.appearanceSwitch.grid(row=0, column=1, sticky='n')
 
         # Set frame dimensions to the screen dimensions
         screenWidth = self.winfo_screenwidth()
         screenHeight = self.winfo_screenheight()
         self.geometry(f"{screenWidth}x{screenHeight}")
+        logger().info(f"Frame Width: {screenWidth}")
+        logger().info(f"Frame Height: {screenHeight}")
+
+        logger().info(f"Successfully started GUI")
+
+    def toggleAppearance(self):
+        """
+        Used to change the appearance of the program
+        """
+        ctk.set_appearance_mode(self.appearance.get())
+        logger().info(f"Appearance changed to {self.appearance.get()}")
 
     def main(self):
         self.mainloop()
